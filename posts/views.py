7e8bd60f92e6eb8c5f1b92691db38105a67c1900
nick bouldien <nick.bouldien@gmail.com>
2017-04-29 14:56:39 -0700

    create view/home page for post

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.utils import timezone
+from . import models
 # Create your views here.
+
+
 @login_required
 def create(request):
-    return render(request, 'posts/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url']:
+            post = models.Post()
+            post.title = request.POST['title']
+            post.url = request.POST['url']
+            post.pub_date = timezone.datetime.now()
+            post.author = request.user
+            post.save()
+            return render(request, 'posts/home.html')
+
+        else:
+            return render(request, 'posts/create.html', {'error': 'Error: You must include a title and a url'})
+
+    else:
+        return render(request, 'posts/create.html')
+
+def home(request):
+    return render(request, 'posts/home.html')
